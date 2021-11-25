69f2579f7de55573d1d1213edb638c9e622c7594
wangfei <1140554608@qq.com>
2021-11-25 21:16:38 +0800

    label generation

---
+++ generate_label.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+@Author     : Fei Wang
+@Contact    : fwang1412@gmail.com
+@Time       : 2021/11/24 18:25
+@Description: 
+"""
+import os
+import inflect
+import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+from utils import load_label_set, load_data
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+DEVICE = 'cuda:0'
+
+
+def run(model_name="bert-base-uncased", output_file='generated_labels.txt', batch_size=64, max_seq_length=128, k=20):
+    labels, sentences = load_data('data/prompts.txt')
+    label_set = load_label_set()
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+    model = AutoModelForMaskedLM.from_pretrained(model_name)
+    model.to(DEVICE)
+    model.eval()
+
+    inflect_engine = inflect.engine()
+
+    generated_labels = []
+    with torch.no_grad():
+        for i in range(0, len(sentences), batch_size):
+            batch_sentences = sentences[i:i + batch_size]
+            batch_inputs = tokenizer(batch_sentences, max_length=max_seq_length, truncation=True)
+            batch_inputs = {key: torch.LongTensor(value).to(DEVICE) for key, value in batch_inputs.items()}
+            mask_indices = (batch_inputs['input_ids'] == 103).nonzero()
+            batch_outputs = model(**batch_inputs)
+            batch_logits = batch_outputs[0].cpu().numpy()
+
+            for j, (logits, mask_index) in enumerate(zip(batch_logits, mask_indices)):
+                assert j == mask_index[0]
+                pred_token_ids = np.argsort(-logits[mask_index[1]].reshape(-1))[:k]  # TODO: other thresholds
+                pred_tokens = tokenizer.convert_ids_to_tokens(pred_token_ids)
+                pred_labels = []
+                for x in pred_tokens:
+                    singular = inflect_engine.singular_noun(x)
+                    if singular is not False:
+                        pred_labels.append(singular)
+                    else:
+                        pred_labels.append(x)
+                valid_labels = set(pred_labels) & label_set
+                generated_labels.append(valid_labels)
+
+    with open(output_file, 'w') as f:
+        for labels in generated_labels:
+            f.write(' '.join(labels) + '\n')
+
+
+if __name__ == "__main__":
+    run()
---
+++ utils.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+@Author     : Fei Wang
+@Contact    : fwang1412@gmail.com
+@Time       : 2021/11/25 18:44
+@Description: 
+"""
+
+
+def load_data(file):
+    labels = []
+    sentences = []
+    with open(file) as f:
+        for line in f.readlines():
+            sample = line.strip().split('\t')
+            labels.append(sample[0].split())
+            sentences.append(sample[1])
+    return labels, sentences
+
+
+def load_label_set(file='data/ontology/types.txt'):
+    labels = set()
+    with open(file) as f:
+        for line in f.readlines():
+            labels.add(line.strip())
+    return labels
+
+
+def load_label_dict(file='data/ontology/types.txt'):
+    label_dict = {}
+    with open(file) as f:
+        for i, line in enumerate(f.readlines()):
+            label_dict[line.strip()] = i
+    return label_dict
