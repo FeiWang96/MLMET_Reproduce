a3bc2d3040bf9f0e5e1204cb4cdb580ad5f19265
WGC575 <wgc575@gmail.com>
2021-11-26 12:18:47 -0800

    [v202111261218_jhe] Added prepare_data.py

---
+++ prepare_data.py
@@ -0,0 +1,11 @@
+import os
+import json
+import gzip
+import re
+import xml.etree.ElementTree as ET
+
+#macros
+pronouns = {'i', 'me', 'myself', 'we', 'us', 'ourselves', 'he', 'him', 'himself', 'she', 'her', 'herself',
+            'it', 'they', 'them', 'themselves', 'you'}
+nn_tags = {'NN', 'NNP', 'NNS', 'NNPS'}
+
