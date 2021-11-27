87f58f3fb46bc4b1aa43d29a98399eb4da18efb1
wangfei <1140554608@qq.com>
2021-11-27 23:31:23 +0800

    update dataflow

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # MLMET_Reproduce
 USC CSCI 662 Project
+
+## Results
+
+### Table 2: Comparison of Patterns
+
+Pattern | Precision | Recall | F1 
+--- | --- | --- | --- |
+M and any other H | 21.8 | 33.1 | 26.3
+M and some other H | 19.8 | 34.4 | 25.1
+H such as M | 21.6 | 23.0 | 22.3
+such H as M | 14.8 | 25.1 | 18.6
+H including M | 22.4 | 15.0 | 18.0
+H especially M | 20.1 | 5.1 | 8.2
+
+* Note that instead of selecting top 10 valid predictions for each entity, we select valid predictions from top 10 predictions. This setting results in higher F1 scores for top 5 patterns.
+
+### Table 4: Comparison of Weakly Labeled Data
+
+Method | Precision | Recall | F1 
+--- | --- | --- | --- |
+BERT-Ultra-Direct | 52.3 | 31.9 | 39.6
+BERT-Ultra-Pre |  |  |  |
+Ours (Single Pattern, Unweighted Loss, No self-train) |  |  |  |
+
+
+
--- generate_label.py
+++ generate_label.py
@@ -6,19 +6,21 @@
 @Description: 
 """
 import os
+from tqdm import tqdm
 import inflect
 import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
-from utils import load_label_set, load_data
+from utils import load_label_set, load_data, macro_f1
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 DEVICE = 'cuda:0'
 
 
-def run(model_name="bert-base-cased", output_file='generated_labels.txt', batch_size=64, max_seq_length=128, k=20):
-    labels, sentences = load_data('data/prompts.txt')
+def run(model_name="bert-base-cased", input_file='data/distant.prompt', output_file='data/distant.mlm',
+        batch_size=64, max_seq_length=128, k=10):
+    gold_labels, sentences = load_data(input_file)
     label_set = load_label_set()
 
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
@@ -29,18 +31,23 @@ def run(model_name="bert-base-cased", output_file='generated_labels.txt', batch_
     inflect_engine = inflect.engine()
 
     generated_labels = []
+    skipped = 0
     with torch.no_grad():
-        for i in range(0, len(sentences), batch_size):
+        for i in tqdm(range(0, len(sentences), batch_size)):
             batch_sentences = sentences[i:i + batch_size]
-            batch_inputs = tokenizer(batch_sentences, max_length=max_seq_length, truncation=True)
+            batch_inputs = tokenizer(batch_sentences, max_length=max_seq_length, truncation=True, padding='max_length')
             batch_inputs = {key: torch.LongTensor(value).to(DEVICE) for key, value in batch_inputs.items()}
-            mask_indices = (batch_inputs['input_ids'] == 103).nonzero()
-            batch_outputs = model(**batch_inputs)
-            batch_logits = batch_outputs[0].cpu().numpy()
 
-            for j, (logits, mask_index) in enumerate(zip(batch_logits, mask_indices)):
-                assert j == mask_index[0]
-                pred_token_ids = np.argsort(-logits[mask_index[1]].reshape(-1))[:k]  # TODO: other thresholds
+            batch_outputs = model(**batch_inputs)
+            batch_logits = batch_outputs[0]
+
+            for j, (logits, input_ids) in enumerate(zip(batch_logits, batch_inputs['input_ids'])):
+                mask_index = (input_ids == 103).nonzero().reshape(-1)
+                if mask_index.nelement() != 1:
+                    generated_labels.append([])
+                    skipped += 1
+                    continue
+                pred_token_ids = torch.topk(logits[mask_index[0]].reshape(-1), k)[1]  # TODO: other thresholds k
                 pred_tokens = tokenizer.convert_ids_to_tokens(pred_token_ids)
                 pred_labels = []
                 for x in pred_tokens:
@@ -52,9 +59,17 @@ def run(model_name="bert-base-cased", output_file='generated_labels.txt', batch_
                 valid_labels = set(pred_labels) & label_set
                 generated_labels.append(valid_labels)
 
-    with open(output_file, 'w') as f:
-        for labels in generated_labels:
-            f.write(' '.join(labels) + '\n')
+    print(f'skip {skipped} sentences')
+
+    if output_file:
+        with open(output_file, 'w') as f:
+            for labels in generated_labels:
+                f.write(' '.join(labels) + '\n')
+
+    true_and_prediction = []
+    for gold, pred in zip(gold_labels, generated_labels):
+        true_and_prediction.append((gold, pred))
+    print('precision, recall, f1', macro_f1(true_and_prediction))
 
 
 if __name__ == "__main__":
